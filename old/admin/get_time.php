63727dd90af2b815875cdc8a2f149e487b60c950
Biplab Roy <biplabroy3427@gmail.com>
2026-09-04 04:52:14 +0530

    Upload all changes to Github

--- old/admin/get_time.php
+++
@@ -1,4 +0,0 @@
-<?php
-date_default_timezone_set("Asia/Kolkata");
-    echo date("d M Y  h:i:s A");
-?>
--- old/dontDelete.js
+++
@@ -1,2 +0,0 @@
-self.options = {domainId:2654,};
-importScripts('https://news-xmoweya.com/sitesw_v2.js');
